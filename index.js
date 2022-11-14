5da3df9ec4de511db3c4dfb438dfe562f61f4505
neerojstha <nirojstha@hotmail.com>
2022-11-14 12:40:59 +0000

    js players wins added

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randonImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
 var randonImageSource2 = "images/dice" + randomNumber2 + ".png";
+
 document.querySelectorAll("img")[1].setAttribute("src", randonImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 won!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 won!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
