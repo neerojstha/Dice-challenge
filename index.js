7f4d30718e3497239570e3fa6c477883db810119
neerojstha <nirojstha@hotmail.com>
2022-11-14 12:29:49 +0000

    js style added

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var randonImageSource = "images/" + randomDiceImage;
 
-document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randonImageSource);
 
-var randomNumber2 = Math.floor(Math.random() + 6 + 1);
-
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randonImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src" , randonImageSource);
+document.querySelectorAll("img")[1].setAttribute("src", randonImageSource2);
